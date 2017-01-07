7096fd633e8adf984db064c9f7f7f725b106b8cd
Bill McCartney <billmccartney@gmail.com>
2017-01-06 23:23:58 -0500

    Added support for threading so the another thread can run the LED writing simultaneously.

--- web/server.py
+++ web/server.py
@@ -1,10 +1,34 @@
+from threading import Thread
+from time import sleep
+running = True
+
+WAKE_ON_LAN = "/usr/bin/wakeonlan"
+
+def background(arg):
+  global running
+  while running:
+    print( "running")
+    sleep(1)
+
 from flask import Flask
+import subprocess
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/wakeonlan/<mac>")
+def wakeonlan(mac):
+  print("mac = ",mac)
+  subprocess.call(WAKE_ON_LAN, mac)
+  return "sent packet"
+
+
 if __name__ == "__main__":
+  thread = Thread(target = background, args = (10, ))
+  thread.start()
   app.run(host= '0.0.0.0', threaded=True)
+  running = False
+  thread.join()
 #    app.run()
